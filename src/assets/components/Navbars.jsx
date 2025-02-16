71233c3cd28f7eb341c48d5effc586ff8eaf75a3
Amna Khan <amna12khan26@gmail.com>
2025-02-16 22:36:46 +0500

    Update Navbars.jsx

--- src/assets/components/Navbars.jsx
+++ src/assets/components/Navbars.jsx
@@ -7,7 +7,7 @@ function Navbars() {
   return (
     <Navbar expand="lg" className="bg-dark justfy-content-end">
       <Container >
-        <Navbar.Brand href="#home" className='text-light'>React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home" className='text-light'>POST APP</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav"  className=" d-flex justify-content-end">
           <Nav className="me-auto">
